a3ddfb931bda3b081f5f71e123d41bcd75067e20
Anna Zamesina <zamesinaanna@gmail.com>
2022-11-19 17:54:29 +0600

    task19 01

---
+++ task19_01/Program.cs
@@ -0,0 +1,25 @@
+﻿// 
+// n=0+1=1 number=1232  
+// n=10+2=12 number=123
+// n=120+3=123 number=12 
+// n=1230+2=1232 number=1 
+// n=12320+1 number =0
+
+Console.WriteLine("Введите число:  ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool Polyndrom(int num)
+{
+    int n = 0;
+    int number = num;
+    while (num > 0)
+    {
+        n = n * 10 + num % 10;
+        num = num / 10;
+    }
+
+    if (n == number) return true;
+    return false;
+}
+
+Console.WriteLine(Polyndrom(number) ? "да" : "нет");
