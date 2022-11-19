71065d272a6c57c05bc4a8ba193abedd93a3427f
Anna Zamesina <zamesinaanna@gmail.com>
2022-11-19 16:08:30 +0600

    add task19

---
+++ task19/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 19
+
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// Выполнить с помощью числовых операций (целочисленное деление, остаток от деления). 25347
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool Polyndrom(int num)
+{
+    if (num / 10000 == num % 10 && num / 1000 % 10 == num / 10 % 10) return true;
+    return false;
+}
+
+if (number < 10000 || number >= 1000000 ) Console.WriteLine("Некорректные данные, повторите ввод");
+else Console.WriteLine(Polyndrom(number) ? "да" : "нет");
+
