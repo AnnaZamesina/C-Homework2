0335b55dce8ee7978ba50b1df4e0a191deb41005
Anna Zamesina <zamesinaanna@gmail.com>
2022-11-22 18:58:47 +0600

    add task29

---
+++ task29/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+Console.WriteLine("Введите длину массива");
+int length = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите максимально допустимое число в массиве");
+int imax = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите минимально допустимое число в массиве");
+int imin = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[length];
+
+void FillArray(int[] collection, int minInd, int maxInd)
+{
+    for (int i = 0; i < collection.Length; i++)
+    {
+        collection[i] = new Random().Next(minInd, maxInd + 1);
+    }
+}
+
+void PrintArray(int[] col)
+{
+    for (int i = 0; i < col.Length; i++)
+    {
+        Console.Write ($"{col[i]}  ");
+    }
+}
+
+FillArray(array, imin, imax);
+PrintArray(array);
