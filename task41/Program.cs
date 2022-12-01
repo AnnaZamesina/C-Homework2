51f915d0be21eeab8aa513aff0be12370f29e8b9
Anna Zamesina <zamesinaanna@gmail.com>
2022-12-01 16:20:35 +0600

    add task 43

--- task41/Program.cs
+++ task41/Program.cs
@@ -2,29 +2,29 @@
 
 // 0, 7, 8, -2, -2 -> 2
 
-Console.WriteLine ("Сколько чисел вы хотите ввести?");
+Console.WriteLine("Сколько чисел вы хотите ввести?");
 int number = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine ($"Введите {number} чисел");
+Console.WriteLine($"Введите {number} чисел");
 
-int [] FillArray (int num)
+int[] FillArray(int num)
 {
-    int [] arr = new int [num];
+    int[] arr = new int[num];
     for (int i = 0; i < num; i++)
     {
         arr[i] = Convert.ToInt32(Console.ReadLine());
     }
     return arr;
-} 
+}
 
-int OverZeroNumbers (int [] arr)
+int OverZeroNumbers(int[] arr)
 {
     int amount = 0;
     for (int i = 0; i < arr.Length; i++)
     {
-        if (arr[i]>0) amount++;
+        if (arr[i] > 0) amount++;
     }
     return amount;
 }
 
-int [] array = FillArray(number);
+int[] array = FillArray(number);
 Console.WriteLine($"Количество введенных чисел больше 0: {OverZeroNumbers(array)}");
---
+++ task43/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Введите коэффициент угла наклона k1 первой прямой");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите коэффициент b1 первой прямой");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите коэффициент угла наклона k2 второй прямой");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите коэффициент b2 второй прямой");
+double b2 = Convert.ToDouble(Console.ReadLine());
+
+double[] FindCrossPoint(double indK1, double indB1, double indK2, double indB2)
+{
+    double[] foundPointArray = new double[2];
+    foundPointArray[0] = Math.Round(((indB2 - indB1) / (indK1 - indK2)), 2, MidpointRounding.ToZero);
+    foundPointArray[1] = Math.Round((indK1 * foundPointArray[0] + indB1), 2, MidpointRounding.ToZero);
+    return foundPointArray;
+}
+
+void PrintArray(double[] arr, string symb1, string symb2)
+{
+    Console.Write(symb1);
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1)
+            Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write(symb2);
+}
+
+double[] resultArray = FindCrossPoint(k1, b1, k2, b2);
+
+Console.WriteLine($"Координаты точки пересечения:");
+PrintArray(resultArray, "(", ")");
